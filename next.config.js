69331bae5b4a6fea4ffdc9d2b2ac1d89cb630891
Vastu 45 <vastu.bhandari45@gmail.com>
2025-07-29 19:46:19 +1000

    Simplify configs to fix routes-manifest.json error

--- next.config.js
+++ next.config.js
@@ -3,14 +3,6 @@ const nextConfig = {
   images: {
     domains: ['localhost', 'res.cloudinary.com', 'images.unsplash.com'],
   },
-  env: {
-    DATABASE_URL: process.env.DATABASE_URL,
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
-    STRIPE_PUBLISHABLE_KEY: process.env.STRIPE_PUBLISHABLE_KEY,
-    STRIPE_WEBHOOK_SECRET: process.env.STRIPE_WEBHOOK_SECRET,
-  },
   experimental: {
     serverComponentsExternalPackages: ['@prisma/client'],
   },
